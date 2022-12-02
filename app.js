1c1961bb902876943255ffaa66f0c2f945d9fd6e
Alexandre Sécher <asecher@iphc.cnrs.fr>
2022-12-02 11:18:52 +0100

    updated layout and stylesheets

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express_m = require('express');
 const path_m = require('node:path');
 const logger_m = require('morgan');
 const index_router = require('./routes/index');
-const update_sender_m = require('./update_sender');
+const discussion_router = require('./routes/discussion');
+const doorway_router = require('./routes/doorway');
+//const update_sender_m = require('./update_sender');
 const app = express_m();
 app.disable('etag').disable('x-powered-by');
 //TODO: add helmet
 
 const mongoose_m = require("mongoose");
-const mongo_db = 'mongodb://localhost:27017/audio_basics';
+const mongo_db = 'mongodb://localhost:27017/invoice';
 mongoose_m.connect( mongo_db );
 const database = mongoose_m.connection;
 database.on( 'error', console.error.bind( console, "MongoDB connection error: ") );
@@ -18,8 +20,10 @@ app.set('view engine', 'pug');
 
 app.use(express_m.static(path_m.join(__dirname, 'public')));
 app.use('/', index_router);
+app.use('/doorway', doorway_router);
+app.use('/discussion', discussion_router);
 
-app.use('/update_sender', update_sender_m ); 
+//app.use('/update_sender', update_sender_m ); 
 
 const https_m = require('node:https');
 const fs_m = require('node:fs');
