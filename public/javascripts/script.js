aecbf50648f69236a072dc1f5e759e882b31b2ab
Alexandre Sécher <asecher@iphc.cnrs.fr>
2022-12-06 15:46:21 +0100

    script.js: force dialog.showModal before loading other modules

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,9 +1,10 @@
-import './modules/sign.js';
-import './modules/discussion.js';
-
 const dialog = document.querySelector('dialog');
 dialog.addEventListener('cancel', event => event.preventDefault() ); 
 dialog.showModal();
+
+import './modules/sign.js';
+import './modules/discussion.js';
+
 
 
 
