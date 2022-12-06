8528cbba2d8e594303d0c930fa6d82dc6c0d5319
Alexandre Sécher <asecher@iphc.cnrs.fr>
2022-12-06 11:39:50 +0100

    updated message_model with url and local_url

--- models/message.js
+++ models/message.js
@@ -4,9 +4,8 @@ const schema_c = mongoose_m.Schema;
 
 const message_schema = new schema_c({
   mimetype: String,
-  discussion: {type: schema_c.Types.ObjectId, ref:'discussion'}, 
-  type: String,
-  author: {type: schema_c.Types.ObjectId, ref:'user'}, 
+  discussion: String, 
+  author: String, 
   visualisation: [Number], 
   track_counter: Number,
 });
@@ -15,12 +14,30 @@ message_schema.virtual('paths').get( function() {
   let index = 0 ;
   const extension = this.mimetype.split('/')[1];
   return [...Array(this.track_counter)].map(
-    () => `.${this.url}${index++}.${extension}`
+    () => `.${this.local_url}${index++}.${extension}`
   );
 });
-message_schema.virtual('url').get( function() {
-  return `/media/${this.discussion}/${this._id}/`;
+message_schema.virtual('local_url').get( function() {
+  return `/discussion/${this.discussion}/${this._id}/`;
 });
+message_schema.method( 'url', 
+function ( index ) {
+  console.log(`message_model.url -> ${this.discussion}`);
+  console.log(`this is: ${this}`);
+  return `/discussion/${this.discussion}/${index}/`;
+});
+message_schema.method( 'update_discussion_a', 
+function() {
+  mongoose_m.model('discussion').findOne({users: this.discussion.split('+')})
+    .then( discussion => {
+      discussion.messages.push( this._id );
+      console.log(discussion);
+      if(discussion.messages.length > 10){ discussion.messages.shift() }
+      //TODO: should also erase corresponding audio files
+      return discussion.save();
+    });
+});
+
 //TODO: receiver is virtual: composed from discussion and author ? but if in alphabetical order? you can find in string, no matter the position
 
 module.exports = mongoose_m.model("message", message_schema);
